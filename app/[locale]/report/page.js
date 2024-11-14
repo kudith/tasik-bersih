5317e05549f538a4ab3c13706a79f97c4507989e
kudith <ginanjar.aditya15@gmail.com>
2024-11-14 20:17:20 +0700

    add report form

---
+++ app/[locale]/report/page.js
@@ -0,0 +1,21 @@
+import { ReportPolluted } from "@/components/ReportPolluted";
+import initTranslations from "../../i18n";
+import TranslationsProvider from "@/components/TranslationsProvider";
+
+const i18nNamespaces = ['report'];
+
+export default async function ReportPage({ params }) {
+  const { locale } = await params;
+  const { resources } = await initTranslations(locale, i18nNamespaces);
+
+  return (
+    <TranslationsProvider
+      namespaces={i18nNamespaces}
+      locale={locale}
+      resources={resources}>
+      <div className="my-20">
+        <ReportPolluted />
+      </div>
+    </TranslationsProvider>
+  );
+}
